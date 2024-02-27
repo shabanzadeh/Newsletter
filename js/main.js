ed7ea512020531810d100e52142eda354c18dd0b
Maryam Shabanzadeh <shabanzadeh1712@yahoo.com>
2024-02-27 15:01:43 +0100

    valided email

--- js/main.js
+++ js/main.js
@@ -1,14 +1,21 @@
 const myform = document.querySelector("#my-form");
 const emailInput = document.querySelector("#email")
+const msg = document.querySelector(".msg")
 //console.log(emailInput.value)
 //const submit_input = document.querySelector(".btn")
-console.log(myform)
 
-myform.addEventListener("submit", onSubmit)
+ 
+
+myform.addEventListener("submit",onSubmit)
 
 function onSubmit(e){
     e.preventDefault()
+    let pattern = /^[a-zA-Z][a-zA-Z0-9\-\_\.]+@[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}$/;
     console.log(emailInput.value)
+    if(emailInput.value==='' || !emailInput.value.match(pattern)){
+        msg.innerHTML = "<h5> vaild email requierded</h5>"
+        msg.classList.add("error")
+    }
 
    
 }
